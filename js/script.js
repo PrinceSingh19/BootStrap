c5fdef1730dd921a5838f32ca48f0d0ec88d5d17
Prince Singh <princesingh808087@gmail.com>
2022-10-03 15:15:06 +0530

    NPM Scripts Part 1 where I added npm-run-all to automate the 2 tasks

---
+++ js/script.js
@@ -0,0 +1,26 @@
+(function(){
+    "use strict";
+    const btnToggle = document.querySelector("#btnModalLogin");
+        const btnToggleReserve = document.querySelector("#btnModalReserve");
+        const btnDismiss = document.querySelectorAll(".dismiss");
+        const btnDismissReserve = document.querySelectorAll(".dismissReserve");
+        const myModalLogin = new bootstrap.Modal(document.getElementById("modalEx"));
+        const myModalReserve = new bootstrap.Modal(document.getElementById("reserve"));
+
+        btnToggleReserve.addEventListener("click", function () {
+            myModalReserve.show();
+        })
+        btnToggle.addEventListener("click", function () {
+            myModalLogin.show();
+        })
+        btnDismiss.forEach(ele => {
+            ele.addEventListener("click", function () {
+                myModalLogin.hide();
+            })
+        });
+        btnDismissReserve.forEach(ele => {
+            ele.addEventListener("click", function () {
+                myModalReserve.hide();
+            })
+        });
+})();
